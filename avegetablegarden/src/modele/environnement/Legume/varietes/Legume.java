a3ee20114b2b4f6aeee055d9885ec2c40de8533a
Artiell <61852335+Artiell@users.noreply.github.com>
2023-03-22 17:17:20 +0100

    Update Legume.java

--- avegetablegarden/src/modele/environnement/Legume/varietes/Legume.java
+++ avegetablegarden/src/modele/environnement/Legume/varietes/Legume.java
@@ -1,10 +1,27 @@
 package modele.environnement.Legume.varietes;
 
+import modele.environnement.Legume.EtatLegume;
+
 public abstract class Legume {
+
+    public EtatLegume getEtatLegume() {
+        return etatLegume;
+    }
+    public void setEtatLegume(EtatLegume etatLegume) {
+        this.etatLegume = etatLegume;
+    }
+    private EtatLegume etatLegume;
+
+
+    protected Legume() {
+        this.etatLegume = EtatLegume.germe;
+    }
+
     public abstract Varietes getVariete();
     public void nextStep() {
         croissance();
     }
+
 
     protected abstract void croissance(); // définir selon les conditions
 }
