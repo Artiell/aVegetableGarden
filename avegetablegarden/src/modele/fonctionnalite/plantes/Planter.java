b55073b619107883734854e597d1dea18cc23f37
lea godot <lele.godot@gmail.com>
2023-04-18 14:59:04 +0200

    .

---
+++ avegetablegarden/src/modele/fonctionnalite/plantes/Planter.java
@@ -0,0 +1,20 @@
+package modele.fonctionnalite.plantes;
+
+import modele.Magasin;
+import modele.environnement.Legume.Legume;
+import modele.fonctionnalite.Fonctionnalite;
+
+public abstract class Planter extends Fonctionnalite {
+    public Planter (){
+        super();
+    }
+    public Fonctionnalite actionUtilisateur (int xx){
+        return switch (xx) {
+            case 0 -> new GraineSalade();
+            case 1 -> new GraineCarotte();
+            case 2 -> new GraineTomate();
+            default -> null;
+        };
+    }
+    public abstract Legume action(Magasin magasin);
+}
